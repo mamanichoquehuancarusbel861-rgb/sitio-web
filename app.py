a809487ef7a1f6b39312d0dad8454937454dc805
rusbel <mamanichoquehuancarusbel861@gmail.com>
2026-07-28 14:54:21 -0500

    Agregar todas las rutas en app.py

--- app.py
+++ app.py
@@ -6,5 +6,25 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
+@app.route('/operaciones')
+def operaciones():
+    return render_template('operaciones.html')
+
+@app.route('/proyectos')
+def proyectos():
+    return render_template('proyectos.html')
+
+@app.route('/empleos')
+def empleos():
+    return render_template('empleos.html')
+
+@app.route('/contacto')
+def contacto():
+    return render_template('contacto.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
